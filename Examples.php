5581deebffe1c3952b0e0da9f43fbaef16d1b43c
mohit-gh <mohitjajani85@gmail.com>
2018-05-03 19:01:05 +0530

    Update Examples.php

--- Examples.php
+++ Examples.php
@@ -281,3 +281,65 @@ Array
 
 ?>
 /*Output : KD */
+
+<?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+
+  class Collection implements Countable, JsonSerializable {
+
+    protected $items = [];
+
+    // __set()
+    // __get()
+    // __call()
+    // __toString()
+
+    public function __set($key, $value) {
+      $this->set($key, $value);
+    }
+
+    public function __get($value) {
+      return $this->get($value);
+    }
+
+    public function add($value) {
+      $this->items[] = $value;
+    }
+
+    public function get($key) {
+      return array_key_exists($key, $this->items) ? $this->items[$key] : null;
+    }
+
+    public function __call($func, $args) {
+      echo $func . 'has been called with arguments' . implode(',', $args);
+    }
+
+    public function set($key, $value) {
+      $this->items[$key] = $value;
+    }
+
+    public function jsonSerialize() {
+      return json_encode($this->items);
+    }
+
+    public function count() {
+      return  count($this->items);
+    }
+
+    public function all() {
+      return $this->items;
+    }
+
+  }
+
+  $c = new Collection();
+  $c->add('Mohit');
+  $c->add('Mayur');
+
+  $c->key3 = 'KD';
+
+  echo $c->Mohit();
+?>
+
+/* Output : Mohithas been called with arguments */
