0193f091d0cee293397cf384761182348bad1936
mohit-gh <mohitjajani85@gmail.com>
2018-05-03 18:51:11 +0530

    Update Examples.php

--- Examples.php
+++ Examples.php
@@ -164,3 +164,59 @@ Array
     [key3] => KD
 )
 */
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+<?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+
+  class Collection implements Countable, JsonSerializable {
+
+    protected $items = [];
+
+    // __set()
+    // __get()
+    // __call()
+    // __toString()
+
+    public function __set($key, $value) {
+      $this->set($key, $value);
+    }
+
+    public function add($value) {
+      $this->items[] = $value;
+    }
+
+    public function get($key) {
+      return array_key_exists($key, $this->items) ? $this->items[$key] : null;
+    }
+
+    public function set($key, $value) {
+      $this->items[$key] = $value;
+    }
+
+    public function jsonSerialize() {
+      return json_encode($this->items);
+    }
+
+    public function count() {
+      return  count($this->items);
+    }
+
+    public function all() {
+      return $this->items;
+    }
+
+  }
+
+  $c = new Collection();
+  $c->add('Mohit');
+  $c->add('Mayur');
+
+  $c->key3 = 'KD';
+
+  echo $c->get('key3');
+
+?>
+/* Output : KD */
